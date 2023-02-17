88f0ddee7b167ef45f1ff43d5464c7f09774a25d
mpinedaUMG <125693812+mpinedaUMG@users.noreply.github.com>
2023-02-17 13:05:11 -0600

    Tarea 1
    Tarea 1 de Analisis de Sistemas1

---
+++ WebAppPrimos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppPrimos.Startup))]
+namespace WebAppPrimos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
